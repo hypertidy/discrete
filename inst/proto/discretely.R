6c43c5d984de60ab5a4a6041ff6be6e1ca898edb
Michael Sumner <mdsumner@gmail.com>
2018-03-21 21:20:57 +1100

    now true to paired edges, included east of bounds :)

--- inst/proto/discretely.R
+++ inst/proto/discretely.R
@@ -2,9 +2,20 @@ library(silicore)
 library(silicate)
 library(sf)
 x <- minimal_mesh[1, ]
-x <- inlandwaters[4, ]
+x <- inlandwaters[1, ]
 #x <- st_as_sf(anglr::simpleworld)
+eget <- function(i = 1) {
+  edges %>% filter(edge == i)
+}
+edraw <- function(i = 1) {
+  eget(i) %>% select(xi, yi) %>% lines(lwd = 3)
+  invisible(NULL)
+}
 
+library(raster)
+r <- raster(extent(x) + 0.5, nrows = 500, ncols = 3000)
+
+#r <- raster(extent(1.5, 3, -0.1, 0.5), nrows = 20, ncols = 30)
 #x <- st_sf(geometry = st_sfc(
 #  st_polygon(list(cbind(c(0, 1, 2, 0), c(0, 1, 0, 0))))
 #))
@@ -18,14 +29,21 @@ xstart <- function(Xstart, Ystart, dxdy, Y) {
 }
 
 
-library(raster)
-r <- raster(extent(x) + 0.5, nrows = 200, ncols = 300)
 
 
 library(dplyr)
 sc <- SC0(x)
-sc$coord <- sc$coord %>% mutate(i = as.integer(colFromX(r, x_)),
-                                j = as.integer(rowFromY(r, y_)))
+
+
+#y0 = (ras.ymax - poly(i, 1))/ras.yres - 0.5;
+#y1 = (ras.ymax - poly(i+1, 1))/ras.yres - 0.5;
+
+#(ymax(r) - y_) / res(r)[2] - 0.5
+extent.data.frame <- function(x) extent(as.matrix(x))
+setMethod("extent", "data.frame", extent.data.frame)
+sc$coord <- sc$coord %>% mutate(i = as.integer(trunc((x_ - xmin(r))/res(r)[1]) + 1),
+                                j = as.integer(1L + trunc(((ymax(r) - y_)/res(r)[2]))))
+
 sc_start <- function(x) {
   x$segment %>%
     inner_join(x$coord %>% mutate(.vx0 = row_number()))%>%
@@ -58,33 +76,51 @@ wideys <- cbind(em[seq(1, nrow(edges), by = 2), ],
 colnames(wideys) <- c(".x0", ".y0", ".x1", ".y1")
 wideys <- as_tibble(wideys)
 wideys$dxdy <- edges$dxdy[seq(1, nrow(edges), by = 2)]
+
+
+
+wideys <- wideys %>% filter(.y0 != .y1)
 plot(extent(0, ncol(r), 0, nrow(r)))
 points(sc$coord[c("i", "j")], pch = ".")
-for (jrow in 1:nrow(r)) {
-  active <- jrow >= wideys$.y0 & jrow <= wideys$.y1
-  if (any(active)) {
-    e0 <- wideys %>% filter(active) %>% select(.x0, .x1, .y0, .y1, dxdy)
-    e0 <- e0[order(pmin(e0$.x0, e0$.x1)), ]
-    e0$X0 <- pmin(e0$.x0, e0$.x1)
-    e0$Y0 <- ifelse(e0$dxdy < 0, e0$.y1, e0$.y0)
-    e0 <- e0 %>% mutate(Xstart = xstart(X0, Y0, dxdy, jrow))
 
-    e0 <- e0 %>% distinct(Xstart, .keep_all= TRUE)
-    if (nrow(e0) > 0 ) {
-     # stop()
-      xints <- e0$Xstart
-      #if (length(xints) == 3) stop()
-if (length(unique(xints)) == 1) next;
-      xints <- matrix(xints, ncol = 2, byrow = TRUE)
-      xints[,1] <- floor(xints[,1])
-      xints[,2] <- ceiling(xints[,2])
-      #if (any(is.na(xints))) stop()
-      #xints[is.na(xints)] <- 1
-      xints <- na.omit(xints)
-     for (j in seq_len(nrow(xints))) {
-        points(cbind(seq(xints[j, 1], xints[j, 2]), jrow), pch = ".")
+plot(raster::union(extent(sc$coord %>% dplyr::select(x_, y_)), extent(r)))
+plot(extent(sc$coord %>% dplyr::select(x_, y_)), add = TRUE)
+polygon(sc$coord[c("x_", "y_")], col = "grey")
+plot(extent(r), add = TRUE, col = "dodgerblue")
+
+
+
+plot(raster::union(extent(sc$coord %>% dplyr::select(i, j)), extent(0, ncol(r), 0, nrow(r))))
+plot(extent(sc$coord %>% dplyr::select(i, j)), add = TRUE)
+polygon(sc$coord[c("i", "j")], col = "grey")
+plot(extent(0, ncol(r), 0, nrow(r)), add = TRUE, col = "dodgerblue")
+
+system.time({
+  l <- vector("list", nrow(r))
+  yrange <- range(c(wideys$.y0, wideys$.y1))
+  for (jrow in min(yrange):max(yrange)) {
+    active <- jrow >= wideys$.y0 & jrow <= wideys$.y1
+    if (any(active)) {
+      e0 <- wideys %>% filter(active) %>% select(.x0, .x1, .y0, .y1, dxdy)
+      e0 <- e0[order(pmin(e0$.x0, e0$.x1)), ]
+      e0$X0 <- pmin(e0$.x0, e0$.x1)
+      e0$Y0 <- ifelse(e0$dxdy < 0, e0$.y1, e0$.y0)
+      e0 <- e0 %>% mutate(Xstart = xstart(X0, Y0, dxdy, jrow))
+
+      # print(nrow(e0))
+      inner <- vector("list", nrow(e0)/2)
+      for (i in seq_len(nrow(e0))) {
+        x_end <- ncol(r)
+        if (i %% 2 == 1) {  ## ODD
+          x_start <- min(c(max(c(1, e0$Xstart[i])), ncol(r)))
+        } else {            ## EVEN
+          x_end <- min(c(max(c(1, e0$Xstart[i])), ncol(r)))
+          #run <- seq(x_start, x_end)
+          #    points(cbind(run, jrow), pch = ".")
+          inner[[i]] <- c(x_start, x_end)
+        }
       }
+      l[[jrow]] <- inner
     }
   }
-}
-
+})
